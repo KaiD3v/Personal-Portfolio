08a9ffe28a68abfc3eadab21c7ac9fd9bd1b1116
Kaique <kaikricardo99@gmail.com>
2024-01-29 20:58:14 -0300

    removing useless dumb shit

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,26 +1,11 @@
 import styles from "./Projects.module.css";
-import projectsData from "../../json/Projects.json";  // Importando diretamente o arquivo JSON
+import projectsData from "../../json/Projects.json";
 import { useState, useEffect } from "react";
 
 const Projects = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const [projects, setProjects] = useState()
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        projectsData // n sei como isso deu certo...
-      } catch (err) {
-        console.error("Erro ao buscar projetos:", err);
-        setError(err);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchData();
-  }, []);
 
   if (loading) {
     return <p>Loading...</p>;
