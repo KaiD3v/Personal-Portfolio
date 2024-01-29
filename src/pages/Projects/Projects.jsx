be883534f32c7c8b54c70307d7d3afd4ed16f691
Kaique <kaikricardo99@gmail.com>
2024-01-29 20:45:29 -0300

    projects page fetching

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,11 +1,47 @@
-import styles from './Projects.module.css'
+import styles from "./Projects.module.css";
+import projectsData from "../../json/Projects.json";  // Importando diretamente o arquivo JSON
+import { useState, useEffect } from "react";
 
 const Projects = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [projects, setProjects] = useState()
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        projectsData // n sei como isso deu certo...
+      } catch (err) {
+        console.error("Erro ao buscar projetos:", err);
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Ocorreu um erro ao carregar os projetos.</p>;
+  }
+
   return (
     <div>
-        <h1>Projects</h1>
+      {projectsData.map((project) => (
+        <div key={project.id}>
+          <img src={project.image} alt={project.title} />
+          <h3>{project.title}</h3>
+          <p>{project.text}</p>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
+ 
